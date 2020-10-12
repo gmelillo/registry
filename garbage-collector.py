3af40979eeb43882fe152f683825d39e20e891d0
Gabriel Melillo <gabriel@melillo.me>
2020-10-12 23:09:21 +0200

    fix(log): added some logging
    Signed-off-by: Gabriel Melillo <gabriel@melillo.me>

--- garbage-collector.py
+++ garbage-collector.py
@@ -1,18 +1,60 @@
 from kubernetes import client, config
 from time import time, sleep
-from json import dumps
 import subprocess, threading, signal, os
-from sys import exit
+from io import StringIO
+import logging
+import json
+import sys
+
+LOG = logging.getLogger()
+
+def setup_logging():
+    class JSONLogFormatter(logging.Formatter):
+        def __init__(self):
+            pass
+        def format(self, record):
+            ret = {}
+            ret.update(record.__dict__)
+            ret['message'] = record.getMessage()
+            for i in ('exc_info', 'exc_text', 'args', 'relativeCreated', 'name', 'thread',
+                      'msecs', 'pathname', 'processName', 'levelno', 'msg', 'stack_info'):
+                if i in ret:
+                    del ret[i]
+            ret['level'] = ret['levelname']
+            del ret['levelname']
+            ret['timestamp'] = int(ret['created'] * 1000)
+            del ret['created']
+            if record.exc_info:
+                if not record.exc_text:
+                    record.exc_text = self.formatException(record.exc_info)
+            if record.exc_text:
+                try:
+                    ret['message'] += record.exc_text
+                except UnicodeError:
+                    # see python's logging formatter for an explanation of this
+                    ret['message'] += record.exc_text.decode(sys.getfilesystemencoding(), 'replace')
+            return json.dumps(ret)
+
+    rootLogger = logging.getLogger()
+    hnd = logging.StreamHandler(sys.stdout)
+    hnd.setFormatter(JSONLogFormatter())
+    rootLogger.setLevel(20)
+    hnd.setLevel(20)
+    rootLogger.addHandler(hnd)
+
 
 class Command(object):
     def __init__(self, cmd):
         self.cmd = cmd
         self.process = None
+        self.log = logging.getLogger(f'{__name__}.{self.__class__.__name__}')
 
     def run(self, timeout):
         def target():
-            self.process = subprocess.Popen(self.cmd, shell=True, preexec_fn=os.setsid)
-            self.process.communicate()
+            self.process = subprocess.Popen(self.cmd, shell=True, preexec_fn=os.setsid, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            process_output, _ = self.process.communicate()
+            for line in str(process_output).split('\n'):
+                self.log.info(str(line))
 
         thread = threading.Thread(target=target)
         thread.start()
@@ -22,10 +64,12 @@ class Command(object):
             os.killpg(self.process.pid, signal.SIGTERM)
             thread.join()
 
+
 class Registry(object):
     def __init__(self, deployment, namespace):
         self.deployment = deployment
         self.namespace = namespace
+        self.log = logging.getLogger(f'{__name__}.{self.__class__.__name__}')
         try:
             config.load_kube_config()
         except:
@@ -47,7 +91,7 @@ class Registry(object):
                     s.observed_generation >= response.metadata.generation):
                 return True
             else:
-                print(f'[updated_replicas:{s.updated_replicas},replicas:{s.replicas}'
+                self.log.info(f'[updated_replicas:{s.updated_replicas},replicas:{s.replicas}'
                     f',available_replicas:{s.available_replicas},observed_generation:{s.observed_generation}] waiting...')
 
         raise RuntimeError(f'Waiting timeout for deployment {self.deployment}')
@@ -67,20 +111,23 @@ class Registry(object):
         self._patch_deployment(deployment)
 
     def readOnly(self, status: bool):
-        self.set_environment('REGISTRY_STORAGE_MAINTENANCE_READONLY', dumps({'enabled': status}))
-        
+        self.set_environment('REGISTRY_STORAGE_MAINTENANCE_READONLY', json.dumps({'enabled': status}))
+
+
 def main():
     try:
         registry = Registry('registry', 'docker')
-        print("Put registry in read only mode")
+        LOG.info("Put registry in read only mode")
         registry.readOnly(True)
+        LOG.info("Running garbage collector")
         Command('/bin/registry garbage-collect --delete-untagged=true /etc/docker/registry/config.yml').run(int(12*60*60))
-        print("Put registry in read/write mode")
+        LOG.info("Put registry in read/write mode")
         registry.readOnly(False)
     except Exception as e:
-        print(e)
-        exit(1)
+        LOG.error(e)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
+    setup_logging()
     main()
