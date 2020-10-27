a31208e88fa87f560221c24be220a53e6a8a16e9
Gabriel Melillo <gabriel@melillo.me>
2020-10-27 11:48:13 +0100

    chore(ci): take versions available from dockerhub
    Signed-off-by: Gabriel Melillo <gabriel@melillo.me>

--- .github/k8s-release.py
+++ .github/k8s-release.py
@@ -1,11 +1,10 @@
 import requests
 from packaging import version
-from os import environ
 import yaml
+import json
 
 versions = {}
-url = 'https://api.github.com/repos/kubernetes/kubernetes/releases'
-headers = {"Authorization": f"token {environ.get('GITHUB_TOKEN')}"}
+url = f'https://hub.docker.com/v2/repositories/kindest/node/tags'
 
 for ch in "OoYyNn":
     if len(yaml.resolver.Resolver.yaml_implicit_resolvers[ch]) == 1:
@@ -14,23 +13,14 @@ for ch in "OoYyNn":
         yaml.resolver.Resolver.yaml_implicit_resolvers[ch] = [x for x in
                 yaml.resolver.Resolver.yaml_implicit_resolvers[ch] if x[0] != 'tag:yaml.org,2002:bool']
 
-while True:
-    response = requests.get(url, headers=headers)
+while url is not None:
     print(url)
-    try:
-        url = [l.split(';')[0].replace('<', '').replace('>', '') for l in response.headers['link'].split(', ') if 'rel="next"' in l][0]
-    except:
-        url = None
-
-    for r in response.json():
-        if r.get('name', None) is not None:
-            if '-alpha' not in r['name'] and '-rc' not in r['name']:
-                minor = '.'.join(r['name'][1:].split('.')[:2])
-                if (minor not in versions or version.parse(versions[minor]) < version.parse(r['name'])) and version.parse(r['name']) > version.parse('1.11'):
-                    versions[minor] = r['name']
-    
-    if url is None:
-        break
+    response = requests.get(url)
+    url = json.loads(response.text.encode('utf-8'))['next']
+    for r in json.loads(response.text.encode('utf-8'))['results']:
+        minor = '.'.join(r['name'][1:].split('.')[:2])
+        if (minor not in versions or version.parse(versions[minor]) < version.parse(r['name'])) and version.parse(r['name']) > version.parse('1.11'):
+            versions[minor] = r['name']
 
 
 for v in sorted(versions.values(), reverse=True):
